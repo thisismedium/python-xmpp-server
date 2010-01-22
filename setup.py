394583715bb052c0740caadbeb828e5ed8301f1e
Ben Weaver <weaver@coptix.com>
2010-01-21 21:05:04 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+setup(
+    name = 'python-xmpp-server',
+    version = '0.1',
+    description = 'An XMPP server.',
+    author = 'Medium',
+    author_email = 'labs@thisismedium.com',
+    license = 'BSD',
+    keywords = 'server xmpp jabber',
+
+    packages = list(find_packages(exclude=('examples', ))),
+    install_requires = ['pycurl==7.16.2.1', 'simplejson', 'tornado', 'lxml']
+)
