4114c25b3283b16afc2a0338a5af56032d8a84e2
Ben Weaver <weaver@coptix.com>
2010-03-01 12:47:27 -0500

    Add some debugging statements to Mechanisms feature.

--- xmpp/features.py
+++ xmpp/features.py
@@ -4,7 +4,7 @@
 """features -- XMPP stream features"""
 
 from __future__ import absolute_import
-import sasl, weakref, random, hashlib, base64
+import sasl, weakref, random, hashlib, base64, logging
 from . import plugin, xml, interfaces as i
 from .prelude import *
 
@@ -93,7 +93,12 @@ class Mechanisms(plugin.Feature):
     ## ---------- Server ----------
 
     def include(self):
-        self.bind(auth=self.begin, abort=self.terminate, success=self.terminate)
+        self.bind(
+            auth=self.begin,
+            abort=self.terminate,
+            success=self.terminate,
+            failure=self.terminate
+        )
         return extend(
             self.E.mechanisms(),
             imap(self.E.mechanism, keys(self.allowed()))
@@ -103,7 +108,7 @@ class Mechanisms(plugin.Feature):
         Mech = get(self.allowed(), elem.get('mechanism'))
         if not Mech:
             return self.failure('invalid-mechanism')
-
+        logging.debug('Begin mechanism: %r.', Mech)
         state = Mech(self.auth).challenge()
         if not state.data and elem.text:
             return self.challenge_loop(state, elem)
@@ -111,6 +116,7 @@ class Mechanisms(plugin.Feature):
             return self.issue_challenge(state)
 
     def challenge_loop(self, state, elem):
+        logging.debug('SASL challenge-loop: %r %r', state, elem.text)
         state = state(self.decode(elem.text))
         if state.failure():
             return self.abort()
@@ -186,6 +192,7 @@ class Mechanisms(plugin.Feature):
         return self.close()
 
     def terminate(self, elem):
+        logging.error('Terminating SASL negotiation: %r.', xml.tostring(elem))
         return self.close()
 
 
